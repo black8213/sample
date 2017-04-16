e49e4750fa849f10f4cb1379fcb323e82a1db842
black8213 <black8213@hotmail.com>
2017-04-16 13:39:10 +0000

    Move user model to models folder

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+get('/', 'StaticPagesController@home')->name('home');
+get('/help', 'StaticPagesController@help')->name('help');
+get('/about', 'StaticPagesController@about')->name('about');
+
+get('/signup', 'UsersController@create')->name('signup');
+
+?>
